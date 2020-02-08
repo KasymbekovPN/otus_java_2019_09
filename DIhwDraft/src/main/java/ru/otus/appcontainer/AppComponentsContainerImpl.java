c3ca237af8ba39804f72534550f1ca61ac8cdf38
PaKas <KasymbekovPN@yandex.ru>
2020-02-08 18:53:33 +0300

    class AppComponentsContainerImpl was implemented

--- DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,22 +1,73 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
-    private final List<Object> appComponents = new ArrayList<>();
+    private static final Set<String> INVALID_APP_COMPONENT_NAMES = new HashSet<>(){{
+        add("");
+    }};
+
+    private final Map<String, Class[]> nameAndParamMatching = new HashMap<>();
+    private final Map<Class, String> classAndNameMatching = new HashMap<>();
+    private final Set<String> appComponentNames = new HashSet<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
+    private void processConfig(Class<?> configClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         checkConfigClass(configClass);
-        // You code here...
+
+        Map<Integer, Set<String>> prepData = new HashMap<>();
+
+        Method[] declaredMethods = configClass.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(AppComponent.class)){
+                AppComponent annotation = declaredMethod.getAnnotation(AppComponent.class);
+                String name = annotation.name();
+                Integer order = annotation.order();
+
+                checkAppComponentName(name);
+
+                appComponentNames.add(name);
+                nameAndParamMatching.put(name, declaredMethod.getParameterTypes());
+
+                if (prepData.containsKey(order)){
+                    prepData.get(order).add(name);
+                } else {
+                    prepData.put(order, new TreeSet<>(){{add(name);}});
+                }
+            }
+        }
+
+        Object config = configClass.getConstructor().newInstance();
+
+        Set<Integer> keys = prepData.keySet();
+        for (Integer key : keys) {
+            Set<String> names = prepData.get(key);
+            for (String name : names) {
+                Class[] params = nameAndParamMatching.get(name);
+                Method declaredMethod = configClass.getDeclaredMethod(name, params);
+                Object[] objects = new Object[params.length];
+                for(int i = 0; i < params.length; i++){
+                    objects[i] = appComponentsByName.get(
+                        classAndNameMatching.get(params[i])
+                    );
+                }
+
+                Object invoke = declaredMethod.invoke(config, objects);
+                appComponentsByName.put(name, invoke);
+                classAndNameMatching.put(declaredMethod.getReturnType(), name);
+            }
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -25,13 +76,21 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
+    private void checkAppComponentName(String appComponentName){
+        if (INVALID_APP_COMPONENT_NAMES.contains(appComponentName)){
+            throw new IllegalArgumentException(String.format("Invalid name %s", appComponentName));
+        } else if (appComponentNames.contains(appComponentName)){
+            throw new IllegalArgumentException(String.format("Duplicate name %s", appComponentName));
+        }
+    }
+
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return getAppComponent(classAndNameMatching.get(componentClass));
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
